8a2b6f2abf4be629945299a9a7ae8e787e40ee8f
ScurryTurry <92820704+ScurryTurry@users.noreply.github.com>
2024-09-30 13:36:40 -0400

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class CtoFConverter {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double tempC = 0.0;
+        double tempF = 0.0;
+        String continueYN = "";
+        String trash ="";
+        boolean done = false;
+
+        do {
+            System.out.print("Enter the temperature in Celsius: ");
+            if (in.hasNextDouble()) {
+                tempC = in.nextDouble();
+                in.nextLine();
+                tempF = (tempC * 1.8) + 32;
+                System.out.println(tempC + " degrees Celsius is " + tempF + " degrees Fahrenheit");
+
+                System.out.print("Continue [Y/N] ");
+                continueYN = in.nextLine();
+
+                if (continueYN.equalsIgnoreCase("N")) {
+                    done = true;
+                }
+            } else {
+                trash = in.nextLine();
+                System.out.println("You must enter a valid number not " + trash);
+            }
+        }while (!done);
+        System.out.println("Thanks for using my temperature converter");
+    }
+}
